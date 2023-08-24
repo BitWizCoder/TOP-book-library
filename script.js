37f07cb022e510f2b926c349d854e8395b0331be
mhmdNoman <mhmdnoman01@gmail.com>
2023-08-25 02:01:22 +0600

    finish submit form

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Elements
 const bookContainer = document.getElementById("book-container");
-const newBook = document.getElementById("new-book");
+const newBookAdd = document.getElementById("new-book");
 
 // form Elements
 const title = document.getElementById("title");
@@ -37,7 +37,7 @@ function addBookToLibrary(title, cover, author, release, excerpt) {
 addBookToLibrary(
   "Dune",
   "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1555447414i/44767458.jpg",
-  "by Frank Herbert",
+  "Frank Herbert",
   "Release date: June 1, 1965",
   "Set on the desert planet Arrakis, Dune is the story of the boy Paul Atreides, heir to a noble"
 );
@@ -45,7 +45,7 @@ addBookToLibrary(
 addBookToLibrary(
   "1984",
   "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1657781256i/61439040.jpg",
-  "by George Orwell",
+  "George Orwell",
   "Release date: June 8, 1949",
   "The new novel by George Orwell is the major work towards which all his previous writing has pointed"
 );
@@ -53,7 +53,7 @@ addBookToLibrary(
 addBookToLibrary(
   "The Martian",
   "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1413706054i/18007564.jpg",
-  "by Andy Weir",
+  "Andy Weir",
   "Release date: September 27, 2011",
   "Six days ago, astronaut Mark Watney became one of the first people to walk on Mars"
 );
@@ -61,7 +61,7 @@ addBookToLibrary(
 addBookToLibrary(
   "Dark Matter ",
   "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1472119680i/27833670.jpg",
-  "by Blake Crouch",
+  "Blake Crouch",
   "Release date: July 26, 2016",
   "A mindbending, relentlessly surprising thriller from the author of the bestselling Wayward Pines trilogy"
 );
@@ -69,7 +69,7 @@ addBookToLibrary(
 addBookToLibrary(
   "Project Hail Mary",
   "https://bestseller.com.ru/static/uploads/posts/2021-05/1621244505_project_hail_mary.webp",
-  "by Andy Weir",
+  "Andy Weir",
   "Release date: May 4, 2021",
   "Ryland Grace is the sole survivor on a desperate, last-chance mission—and if he fails, humanity and the earth itself will perish"
 );
@@ -77,47 +77,22 @@ addBookToLibrary(
 addBookToLibrary(
   "The Lean Startup",
   "https://kbimages1-a.akamaihd.net/5c5e77cc-1fb9-410b-a735-de95a9a5dd40/1200/1200/False/the-lean-startup-1.jpg",
-  "by Eric Ries",
+  "Eric Ries",
   "Release date: January 1, 2011",
   "Most startups fail. But many of those failures are preventable. The Lean Startup is a new"
 );
 
-// Get book data from const myLibrary array and render on the dom.
-myLibrary.forEach((book) => {
-  const bookElement = `
-  <div class="border-2 border-gray-50 flex p-2">
-    <div class="mr-5 max-w-[125px]">
-      <img
-        src="${book.cover}"
-        alt=""
-      />
-    </div>
-    <div class="flex flex-col gap-2">
-      <h2 class="font-semibold text-xl">
-        ${book.title}
-      </h2>
-      <h3 class="font-semibold text-sm">by ${book.author}</h3>
-      <p class="text-gray-400 text-sm">${book.release}</p>
-      <p class="text-sm">
-        ${book.excerpt}
-        <a href="#" class="text-green-500">...more</a>
-      </p>
-    </div>
-  </div>
-  `;
-  const div = document.createElement("div");
-  div.innerHTML = bookElement;
-
-  bookContainer.appendChild(div);
-});
+// book HTML CARD
 
 // add new book to the array
-newBook.addEventListener("click", () => {
+newBookAdd.addEventListener("click", () => {
   const bookTitle = title.value;
   const bookCover = cover.value;
   const bookAuthor = author.value;
   const bookRelease = release.value;
   const bookExcerpt = excerpt.value;
+
+  // addBookToLibrary(bookTitle, bookCover, bookAuthor, bookRelease, bookExcerpt);
 
   const bookElement = `
   <div class="border-2 border-gray-50 flex p-2">
@@ -144,6 +119,37 @@ newBook.addEventListener("click", () => {
   div.innerHTML = bookElement;
 
   bookContainer.prepend(div);
-
-  console.log(bookTitle, bookCover, bookAuthor, bookRelease, bookExcerpt);
 });
+
+// Get book data from const myLibrary array and render on the dom.
+function renderBooks() {
+  myLibrary.forEach((book) => {
+    const bookElement = `
+    <div class="border-2 border-gray-50 flex p-2">
+      <div class="mr-5 max-w-[125px]">
+        <img
+          src="${book.cover}"
+          alt=""
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <h2 class="font-semibold text-xl">
+          ${book.title}
+        </h2>
+        <h3 class="font-semibold text-sm">by ${book.author}</h3>
+        <p class="text-gray-400 text-sm">${book.release}</p>
+        <p class="text-sm">
+          ${book.excerpt}
+          <a href="#" class="text-green-500">...more</a>
+        </p>
+      </div>
+    </div>
+    `;
+    const div = document.createElement("div");
+    div.innerHTML = bookElement;
+
+    bookContainer.prepend(div);
+  });
+}
+
+renderBooks();
